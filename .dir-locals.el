df2b583fe97437fee36dee54caae7e0f5ade7577
Carl Xiong <xiongc05@gmail.com>
2015-11-02 16:41:53 +0800

    Emacs local conf for this project

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  ;; (mode . js)
+  (indent-tabs-mode . t)
+  (tab-width . 4)
+  (js2-basic-offset . 4)
+  ;; (fill-column . 80)
+  ))
+
